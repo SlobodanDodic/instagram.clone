6754c27b3c31f3dd22b6f8f24ca9ce1f0a9893ba
SlobodanDodic <slobodandodic@gmail.com>
2022-11-21 20:55:45 +0100

    auth & prisma setup

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(){
+    return {message: "Sign-up was succesfull!"}
+  }
+  async signin(){
+    return {message: "Sign-in was succesfull!"}
+  }
+  async signout(){
+    return {message: "Sign-out was succesfull!"}
+  }
+
+}
