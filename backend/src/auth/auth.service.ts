92eb697c97bced8e92270f1206deb8f24abd43b4
SlobodanDodic <slobodandodic@gmail.com>
2022-11-21 22:33:39 +0100

    auth dto setup & prisma setup

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async signup(){
+  async signup(dto: AuthDto){
     return {message: "Sign-up was succesfull!"}
   }
-  async signin(){
+  async signin(dto: AuthDto){
     return {message: "Sign-in was succesfull!"}
   }
-  async signout(){
+  async signout(dto: AuthDto){
     return {message: "Sign-out was succesfull!"}
   }
 
